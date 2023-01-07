a29c7e05a7c1a99e2cc461d2b50cc6588bb53f27
Michael Ekstrand <md@ekstrandom.net>
2023-01-07 09:18:13 -0700

    logging refactor

--- lib/logging.tcl
+++ lib/logging.tcl
@@ -2,6 +2,8 @@ package provide logging 0.1
 package require ansifmt
 
 namespace eval logging {
+    namespace export msg
+
     variable verbose 0
 
     variable lvl_verb
@@ -13,14 +15,22 @@ namespace eval logging {
     variable lvl_alias
     set lvl_alias(err) error
 
-    variable lvl_format
-    set lvl_format(debug) {[ansi::fmt -dim]DBG: $msg[ansi::fmt -reset]}
-    set lvl_format(info) {[ansi::fmt -bold -fg blue]MSG:[ansi::fmt -fg default] $msg[ansi::fmt -reset]}
-    set lvl_format(warn) {[ansi::fmt -bold -fg yellow]WRN: $msg[ansi::fmt -reset]}
-    set lvl_format(error) {[ansi::fmt -bold -fg red]ERR: $msg[ansi::fmt -reset]}
+    proc fmt_debug {msg} {
+        return "[ansi::fmt -dim]DBG: $msg[ansi::fmt -reset]"
+    }
+    proc fmt_info {msg} {
+        return "[ansi::fmt -bold -fg blue]MSG:[ansi::fmt -fg default] $msg[ansi::fmt -reset]"
+    }
+    proc fmt_warn {msg} {
+        return "[ansi::fmt -bold -fg yellow]WRN: $msg[ansi::fmt -reset]"
+    }
+    proc fmt_error {msg} {
+        return "[ansi::fmt -bold -fg red]ERR: $msg[ansi::fmt -reset]"
+    }
 
-    proc configure {flag} {
+    proc configure {flag {arg ""}} {
         variable verbose
+        variable lvl_verb
         switch -- $flag {
             -verbose {
                 set verbose 1
@@ -28,44 +38,53 @@ namespace eval logging {
             -quiet {
                 set verbose -1
             }
+            -level {
+                set verbose $lvl_verb($arg)
+            }
             default {
                 error "unrecognized option $flag"
             }
         }
     }
+
+    proc msg {code args} {
+        variable lvl_alias
+        variable lvl_verb
+        variable verbose
+
+        set level info
+        set fmt ""
+        # check for a level argument
+        switch -glob -- $code {
+            -success {
+                set fmt [ansi::fmt -fg green]
+            }
+            -result {
+                set fmt [ansi::fmt -reset]
+            }
+            -* {
+                set level [string range $code 1 end]
+            }
+            default {
+                set args [linsert $args 0 $code]
+            }
+        }
+
+        set msg "$fmt[join $args]"
+
+        if {[info exists lvl_alias($level)]} {
+            set level $lvl_alias($level)
+        }
+
+        if {![info exists lvl_verb($level)]} {
+            return -code error -errorcode bad-level "invalid logging level $level"
+        }
+
+        if {$lvl_verb($level) <= $verbose} {
+            set fmt_proc "fmt_$level"
+            puts stderr [$fmt_proc $msg]
+        }
+    }
 }
 
-proc msg {code args} {
-    set level info
-    set fmt ""
-    # check for a level argument
-    switch -glob -- $code {
-        -success {
-            set fmt [ansi::fmt -fg green]
-        }
-        -result {
-            set fmt [ansi::fmt -reset]
-        }
-        -* {
-            set level [string range $code 1 end]
-        }
-        default {
-            set args [linsert $args 0 $code]
-        }
-    }
-
-    set msg "$fmt[join $args]"
-
-    if {[info exists ::logging::lvl_alias($level)]} {
-        set level $::logging::lvl_alias($level)
-    }
-
-    if {![info exists ::logging::lvl_verb($level)]} {
-        return -code error -errorcode bad-level "invalid logging level $level"
-    }
-
-    if {$::logging::verbose >= $::logging::lvl_verb($level)} {
-        set fmt [subst $::logging::lvl_format($level)]
-        puts stderr $fmt
-    }
-}
+namespace import logging::msg
