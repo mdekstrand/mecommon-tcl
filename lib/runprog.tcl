7e70a26f31957456a1a42610293617705bdf821e
Michael Ekstrand <md@ekstrandom.net>
2023-01-07 22:25:06 -0700

    support noout in run

--- lib/runprog.tcl
+++ lib/runprog.tcl
@@ -5,11 +5,17 @@ package provide runprog 1.0
 package require logging
 
 proc run {args} {
+    set out @stdout
+    if {[string equal [lindex $args 0] -noout]} {
+        set out /dev/null
+        lshift args
+    }
+
     set disp $args
     if {[llength $disp] > 10} {
         set disp [lrange $disp 0 9]
         lappend disp "..."
     }
     msg -debug "running command: $disp"
-    exec {*}$args >@stdout 2>@stderr
+    exec {*}$args >$out 2>@stderr
 }
