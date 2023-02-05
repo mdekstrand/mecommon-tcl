3355c9ffd8e8566d87948ff4d330b787fbcd2659
Michael Ekstrand <md@ekstrandom.net>
2023-02-05 00:42:54 -0600

    uri encode test

--- lib/uri.tcl
+++ lib/uri.tcl
@@ -13,13 +13,27 @@ proc uri::parse_abs {uri} {
     }
 }
 
-proc uri::encode {uri} {
+proc uri::encode {data} {
+    set n [string length $data]
+    set string ""
+    for {set i 0} {$i < $n} {incr i} {
+        set char [string index $data $i]
+        if {[string is alnum $char] || $char in {- _ . ~}} {
+            append string $char
+        } elseif {$char eq " "} {
+            append string %20
+        } else {
+            scan char %c cno
+            append string [format "%%%02d" $cno]
+        }
+    }
+    return $string
 }
 
 proc uri {cmd args} {
     set commands [list encode]
     if {$cmd in $commands} {
-        return [uplevel 1 uri::$cmd {*}$args]
+        return [uri::$cmd {*}$args]
     } else {
         error "unknown uri subcommand $cmd"
     }
