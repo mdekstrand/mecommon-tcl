d857e8ec05ef5a143e55ee39020b707d21ff9b4a
Michael Ekstrand <md@ekstrandom.net>
2023-02-12 17:35:17 -0700

    implement and test fs walking

---
+++ fswalk.tcl
@@ -0,0 +1,36 @@
+#!/usr/bin/env jimsh
+#
+# Test program for fsextra's walk
+
+set root [file dirname [info script]]
+set auto_path [list $root/lib {*}$auto_path]
+
+package require logging
+package require getopt
+package require fsextra
+
+set walk_args [list]
+
+getopt arg $argv {
+    -v - --verbose {
+        # increase logging verbosity
+        logging::configure -verbose
+    }
+
+    --dirs-last {
+        # print directories after their contents
+        lappend walk_args -dirs-last
+    }
+    --no-dirs {
+        # print directories after their contents
+        lappend walk_args -no-dirs
+    }
+
+    arglist {
+        set paths $arg
+    }
+}
+
+fswalk {*}$walk_args path $paths {
+    msg "visted $path"
+}
--- lib/fsextra.tcl
+++ lib/fsextra.tcl
@@ -21,3 +21,72 @@ proc fcopy {f1 f2} {
         msg -debug "$f2 up to date"
     }
 }
+
+proc fswalk {args} {
+    set dirs first
+    while {![lempty $args]} {
+        set arg [lshift args]
+        switch -- $arg {
+            -dirs {
+                set dirs [lshift args]
+            }
+            -dirs-last {
+                set dirs last
+            }
+            -no-dirs {
+                set dirs skip
+            }
+            -- {
+                break
+            }
+            -* {
+                error "unrecognized option $arg"
+            }
+            default {
+                # we are done
+                break
+            }
+        }
+    }
+    set var $arg
+    set paths [lshift args]
+    set body [lshift args]
+
+    # alias the variable the body will need
+    upvar $var cur_path
+    
+    # now work the process - the stack maintains the current work list
+    set stack [lmap path $paths {list visit $path}]
+    msg -debug "scanning [llength $stack] paths"
+    while {![lempty $stack]} {
+        set next [lshift stack]
+        lassign $next action path
+        msg -trace "scan: $action $path"
+        switch -- $action {
+            invoke {
+                set cur_path $path
+                uplevel $body
+            }
+            visit {
+                if {[file isdirectory $path]} {
+                    set path [regsub {/*$} $path ""]
+                    set kids [lsort [glob $path/*]]
+                    # since we use a (weird) stack, last needs to be pushed first
+                    if {$dirs eq "last"} {
+                        lunshift stack [list invoke "$path/"]
+                    }
+                    lunshift stack {*}[lmap kp $kids {list visit $kp}]
+                    if {$dirs eq "first"} {
+                        lunshift stack [list invoke "$path/"]
+                    }
+                } else {
+                    lunshift stack [list invoke $path]
+                }
+            }
+        }
+    }
+}
+
+proc copytree {src dst} {
+    
+}
