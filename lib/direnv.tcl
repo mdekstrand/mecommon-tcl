2c9804aaf0ce491db8ab9eb2767f9b7f21a36810
Michael Ekstrand <md@ekstrandom.net>
2023-11-19 08:46:30 -0500

    use namespace ensemble for tagline

--- lib/direnv.tcl
+++ lib/direnv.tcl
@@ -1,19 +1,22 @@
 package provide direnv 0.1
 package require logging
 
-proc {direnv apply} {} {
-    set data [exec direnv export json 2>@stderr]
-    if {[string trim $data] eq ""} {
-        msg -debug "environment string is empty"
-        return
+namespace eval direnv {
+    proc apply {} {
+        set data [exec direnv export json 2>@stderr]
+        if {[string trim $data] eq ""} {
+            msg -debug "environment string is empty"
+            return
+        }
+        msg -trace "environment: $data"
+        set vars [json::decode $data]
+        msg "applying directory environment"
+        foreach {name value} $vars {
+            msg -debug "setting env var $name"
+            set ::env($name) $value
+        }
     }
-    msg -trace "environment: $data"
-    set vars [json::decode $data]
-    msg "applying directory environment"
-    foreach {name value} $vars {
-        msg -debug "setting env var $name"
-        set ::env($name) $value
-    }
-}
 
-ensemble direnv
+    namespace export apply
+    namespace ensemble create
+}
